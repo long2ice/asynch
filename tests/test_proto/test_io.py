d0f0e0c5dd81a426b7f43a0ecaaff509234cc749
Ivan Krivosheev <ikrivosheev@ptsecurity.com>
2021-03-24 13:19:09 +0300

    Add test for BufferReader

--- tests/test_proto/test_io.py
+++ tests/test_proto/test_io.py
@@ -3,6 +3,7 @@ from asyncio import StreamReader
 import pytest
 
 from asynch.proto.io import BufferedReader
+from asynch.proto.io import BufferedWriter
 
 
 @pytest.mark.asyncio
@@ -16,3 +17,17 @@ async def test_BufferedReader_overflow():
     result = await reader.read_bytes(4)
 
     assert result == stream_data
+
+
+@pytest.mark.asyncio
+async def test_BufferedWriter_overflow(mocker):
+    writer = mocker.Mock()
+    writer.drain = mocker.AsyncMock()
+    reader = BufferedWriter(writer, 1)
+
+    await reader.write_bytes(b"1234")
+
+    writer.write.assert_called_with(b"1234")
+    writer.drain.assert_awaited_once()
+    assert reader.position == 0
+    assert len(reader.buffer) == 0
