50b9d6a68a61dc651572fe109b3b5f4d0f380468
Ivan Krivosheev <ikrivosheev@ptsecurity.com>
2021-04-07 17:20:05 +0300

    Fix style

--- asynch/proto/io.py
+++ asynch/proto/io.py
@@ -47,13 +47,13 @@ class BufferedWriter:
             await self.write_bytes(packet)
 
     async def write_fixed_strings(self, data, length):
-        buffer = bytearray(b'\0' * length)
+        buffer = bytearray(b"\0" * length)
         for item in data:
             if isinstance(item, str):
                 packet = item.encode()
             else:
                 packet = item
-            buffer[:len(packet)] = packet
+            buffer[: len(packet)] = packet
             await self.write_bytes(buffer)
 
     async def close(self):
