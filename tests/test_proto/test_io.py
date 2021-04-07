e80917ee6ea570ad7bb1d3bbdf7fe6187ae9e3bc
Ivan Krivosheev <ikrivosheev@ptsecurity.com>
2021-04-07 17:27:32 +0300

    Add test for write_fixed_strings

--- tests/test_proto/test_io.py
+++ tests/test_proto/test_io.py
@@ -22,11 +22,25 @@ async def test_BufferedReader_overflow():
 async def test_BufferedWriter_overflow(mocker):
     writer = mocker.Mock()
     writer.drain = mocker.AsyncMock()
-    reader = BufferedWriter(writer, 1)
+    b_writer = BufferedWriter(writer, 1)
 
-    await reader.write_bytes(b"1234")
+    await b_writer.write_bytes(b"1234")
 
     writer.write.assert_called_with(b"1234")
     writer.drain.assert_awaited_once()
-    assert reader.position == 0
-    assert len(reader.buffer) == 0
+    assert b_writer.position == 0
+    assert len(b_writer.buffer) == 0
+
+
+@pytest.mark.asyncio
+async def test_ByfferedWriter_write_fixed_strings(mocker):
+    writer = mocker.Mock()
+    writer.drain = mocker.AsyncMock()
+    
+    b_writer = BufferedWriter(writer, 1024)
+
+    await b_writer.write_fixed_strings(['', '12', b'12'], 2)
+
+    assert b_writer.buffer == b'\x00\x001212'
+
+
