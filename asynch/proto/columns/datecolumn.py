ce71058964accb22e705c4b4b045b53e47f69466
long2ice <long2ice@gmail.com>
2024-02-21 16:25:28 +0800

    fix: date column. (#92）

--- asynch/proto/columns/datecolumn.py
+++ asynch/proto/columns/datecolumn.py
@@ -1,41 +1,46 @@
 from datetime import date, timedelta
 
-import ciso8601
-
 from .base import FormatColumn
 
+
 epoch_start = date(1970, 1, 1)
+epoch_end = date(2149, 6, 6)
+
+epoch_start_date32 = date(1900, 1, 1)
+epoch_end_date32 = date(2283, 11, 11)
 
 
 class DateColumn(FormatColumn):
-    ch_type = "Date"
-    py_types = (date,)
-    format = "H"
+    ch_type = 'Date'
+    py_types = (date, )
+    format = 'H'
 
-    epoch_start = epoch_start
-    epoch_end = date(2105, 12, 31)
+    min_value = epoch_start
+    max_value = epoch_end
 
-    date_lut = {x: epoch_start + timedelta(x) for x in range(65535)}
+    date_lut_days = (epoch_end - epoch_start).days + 1
+    date_lut = {x: epoch_start + timedelta(x) for x in range(date_lut_days)}
     date_lut_reverse = {value: key for key, value in date_lut.items()}
 
     def before_write_items(self, items, nulls_map=None):
         null_value = self.null_value
 
         date_lut_reverse = self.date_lut_reverse
-        epoch_start = self.epoch_start
-        epoch_end = self.epoch_end
+        min_value = self.min_value
+        max_value = self.max_value
 
         for i, item in enumerate(items):
             if nulls_map and nulls_map[i]:
                 items[i] = null_value
                 continue
 
-            if isinstance(item, str):
-                item = ciso8601.parse_datetime(item).date()
-            if item > epoch_end or item < epoch_start:
-                items[i] = 0
-            else:
+            if item is not date:
+                item = date(item.year, item.month, item.day)
+
+            if min_value <= item <= max_value:
                 items[i] = date_lut_reverse[item]
+            else:
+                items[i] = 0
 
     def after_read_items(self, items, nulls_map=None):
         date_lut = self.date_lut
@@ -44,5 +49,21 @@ class DateColumn(FormatColumn):
             return tuple(date_lut[item] for item in items)
         else:
             return tuple(
-                (None if is_null else date_lut[items[i]]) for i, is_null in enumerate(nulls_map)
+                (None if is_null else date_lut[items[i]])
+                for i, is_null in enumerate(nulls_map)
             )
+
+
+class Date32Column(DateColumn):
+    ch_type = 'Date32'
+    format = 'i'
+
+    min_value = epoch_start_date32
+    max_value = epoch_end_date32
+
+    date_lut_days = (epoch_end_date32 - epoch_start).days + 1
+    date_lut = {
+        x: epoch_start + timedelta(x)
+        for x in range((epoch_start_date32 - epoch_start).days, date_lut_days)
+    }
+    date_lut_reverse = {value: key for key, value in date_lut.items()}
