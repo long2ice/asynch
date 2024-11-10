3cc6fd3eefb0abcf9231c265a20087fe66920b36
Nils <nils.borrmann@tacto.ai>
2024-11-10 18:15:26 +0100

    format

--- tests/test_reconnection.py
+++ tests/test_reconnection.py
@@ -17,11 +17,12 @@ TIMEOUT_SECONDS = 1
 @pytest.fixture(params=["graceful", "ungraceful"])
 async def proxy(request):
     """Start a proxy server from port 9001 to 9000 that kills the connection after it idled for TIMEOUT_SECONDS seconds.
-    It can kill the connection either gracefully (TCP FIN packet), or ungracefully (TCP RST packet)."""
+    It can kill the connection either gracefully (TCP FIN packet), or ungracefully (TCP RST packet).
+    """
 
     handler = functools.partial(handle_proxy, 9000, request.param == "graceful")
     server = await asyncio.start_server(handler, host="localhost", port=9001)
-    logger.info('Started proxy server')
+    logger.info("Started proxy server")
     async with server:
         try:
             server = asyncio.create_task(server.serve_forever())
@@ -62,7 +63,7 @@ async def test_close_disconnected_connection(proxy_pool):
 async def reader_to_writer(name: str, graceful: bool, reader: StreamReader, writer: StreamWriter):
     while True:
         try:
-            data = await asyncio.wait_for(reader.read(2 ** 12), timeout=TIMEOUT_SECONDS)
+            data = await asyncio.wait_for(reader.read(2**12), timeout=TIMEOUT_SECONDS)
 
             if not data:
                 break
@@ -75,17 +76,18 @@ async def reader_to_writer(name: str, graceful: bool, reader: StreamReader, writ
 
     if not graceful:
         sock = writer.get_extra_info("socket")
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER, struct.pack('ii', 1, 0))
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER, struct.pack("ii", 1, 0))
 
     writer.close()
     await writer.wait_closed()
-    logger.info(f'{name} Closed connection to writer.')
+    logger.info(f"{name} Closed connection to writer.")
 
 
-async def handle_proxy(dst_port: int, graceful: bool, src_reader: StreamReader, src_writer: StreamWriter):
-    dst_reader, dst_writer = await asyncio.open_connection('localhost', dst_port)
-    logger.info(f'Opened connection to {dst_port}')
-    src_dst = reader_to_writer('SRC->DST', graceful, src_reader, dst_writer)
-    dst_src = reader_to_writer('DST->SRC', graceful, dst_reader, src_writer)
+async def handle_proxy(
+    dst_port: int, graceful: bool, src_reader: StreamReader, src_writer: StreamWriter
+):
+    dst_reader, dst_writer = await asyncio.open_connection("localhost", dst_port)
+    logger.info(f"Opened connection to {dst_port}")
+    src_dst = reader_to_writer("SRC->DST", graceful, src_reader, dst_writer)
+    dst_src = reader_to_writer("DST->SRC", graceful, dst_reader, src_writer)
     await asyncio.gather(src_dst, dst_src)
-
