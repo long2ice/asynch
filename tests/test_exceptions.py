e7b095f1faa701e66a55b6b2b297ee00754ac1a0
Nils Borrmann <165771308+nils-borrmann-tacto@users.noreply.github.com>
2025-11-29 12:25:37 +0100

    Prevent disconnect on server exception (#10)

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,5 +1,6 @@
 import pytest
 
+from asynch import Connection
 from asynch.errors import ServerException
 from asynch.pool import Pool
 
@@ -11,3 +12,16 @@ async def test_database_exists(config):
             async with conn.cursor() as cursor:
                 with pytest.raises(ServerException):
                     await cursor.execute("create database test")
+
+
+@pytest.mark.asyncio
+async def test_connection_does_not_close_after_exception():
+    async with Connection() as conn:
+        async with conn.cursor() as cur:
+            with pytest.raises(ServerException):
+                await cur.execute("foo")
+
+            assert conn._connection.connected is True
+            assert conn.opened is True
+
+            await cur.execute("select 1")
