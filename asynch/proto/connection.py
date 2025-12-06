103a22f9f38d93ea17f4ad472665eccba8678171
Kaiju Bacon <baconfield@protonmail.com>
2025-12-05 22:36:25 -0600

    Revert #141

--- asynch/proto/connection.py
+++ asynch/proto/connection.py
@@ -36,7 +36,7 @@ from asynch.proto.settings import write_settings
 from asynch.proto.streams.block import BlockReader, BlockWriter
 from asynch.proto.streams.buffered import BufferedReader, BufferedWriter
 from asynch.proto.utils.escape import escape_params
-from asynch.proto.utils.helpers import chunks, column_chunks, query_is_format_style
+from asynch.proto.utils.helpers import chunks, column_chunks
 
 logger = logging.getLogger(__name__)
 
@@ -806,8 +806,6 @@ class Connection:
         if not isinstance(params, Mapping):
             raise ValueError("Parameters are expected to be a mapping")
 
-        if query_is_format_style(query):
-            return query.format(**escape_params(params))
         return query % escape_params(params)
 
     async def process_insert_query(
--- asynch/proto/utils/helpers.py
+++ asynch/proto/utils/helpers.py
@@ -1,5 +1,4 @@
 from itertools import islice, tee
-from string import Formatter
 
 
 def chunks(seq, n):
@@ -30,8 +29,3 @@ def pairwise(iterable):
     a, b = tee(iterable)
     next(b, None)
     return zip(a, b)
-
-
-def query_is_format_style(query: str) -> bool:
-    """Validate that query uses {name} style formatting"""
-    return any(name for _, name, *_ in Formatter().parse(query) if name)
